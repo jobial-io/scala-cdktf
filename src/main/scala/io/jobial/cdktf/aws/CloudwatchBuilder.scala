c40e130e0fc8e7125f2c913a5f500611a1afa487
orbang <orbang@jobial.io>
2023-11-16 14:35:00 +0000

    Added tagging for log groups

--- src/main/scala/io/jobial/cdktf/aws/CloudwatchBuilder.scala
+++ src/main/scala/io/jobial/cdktf/aws/CloudwatchBuilder.scala
@@ -1,14 +1,22 @@
 package io.jobial.cdktf.aws
 
 import com.hashicorp.cdktf.providers.aws.cloudwatch_log_group.CloudwatchLogGroup
+import io.jobial.cdktf.aws.TerraformStackBuildContext.NameTag
+
+import scala.collection.JavaConverters._
 
 trait CloudwatchBuilder extends TerraformStackBuilderCore {
 
-  def addLogGroup[D](name: String, skipDestroy: Boolean = true) = buildAndAddResource[D, CloudwatchLogGroup] { context =>
+  def addLogGroup[D](
+    name: String,
+    skipDestroy: Boolean = true,
+    tags: Map[String, String] = Map()
+  ) = buildAndAddResource[D, CloudwatchLogGroup] { context =>
     CloudwatchLogGroup.Builder
       .create(context.stack, s"$name-loggroup")
       .name(name)
       .skipDestroy(skipDestroy)
+      .tags(context.mergeTags(name, tags).asJava)
   }
 
 }
