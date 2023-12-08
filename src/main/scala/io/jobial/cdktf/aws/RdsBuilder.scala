8db0da3db1f17c03ff48f5135d1e7cb584eff1f7
orbang <orbang@jobial.io>
2023-12-08 04:20:46 +0000

    Aurora serverless v2 support

--- src/main/scala/io/jobial/cdktf/aws/RdsBuilder.scala
+++ src/main/scala/io/jobial/cdktf/aws/RdsBuilder.scala
@@ -3,37 +3,44 @@ package io.jobial.cdktf.aws
 import com.hashicorp.cdktf.providers.aws.db_instance.DbInstance
 import com.hashicorp.cdktf.providers.aws.db_subnet_group.DbSubnetGroup
 import com.hashicorp.cdktf.providers.aws.rds_cluster.RdsCluster
+import com.hashicorp.cdktf.providers.aws.rds_cluster.RdsClusterScalingConfiguration
+import com.hashicorp.cdktf.providers.aws.rds_cluster.RdsClusterServerlessv2ScalingConfiguration
 import com.hashicorp.cdktf.providers.aws.rds_cluster_instance.RdsClusterInstance
 import com.hashicorp.cdktf.providers.aws.rds_cluster_parameter_group.RdsClusterParameterGroup
+import com.hashicorp.cdktf.providers.aws.rds_cluster_parameter_group.RdsClusterParameterGroupParameter
 
 import scala.collection.JavaConverters._
 
 trait RdsBuilder extends IamBuilder {
 
   def addDbSubnetGroup[D](
-    name: String
+    name: String,
+    subnetIds: List[String]
   ) =
     buildAndAddResource[D, DbSubnetGroup] { context =>
       DbSubnetGroup.Builder
         .create(context.stack, s"$name-db-subnet-group")
-        .subnetIds(cbtechAllSubnets("dev").asJava)
+        .subnetIds(subnetIds.asJava)
     }
-
-
-  def addRdsCluster1[D](
+  
+  def addRdsCluster[D](
     name: String,
     engine: String,
+    engineMode: Option[String],
     availabilityZones: List[String],
     masterUsername: String,
     masterPassword: String,
     subnetGroup: DbSubnetGroup,
     clusterParameterGroup: RdsClusterParameterGroup,
+    scalingConfiguration: Option[RdsClusterScalingConfiguration] = None,
+    serverlessv2ScalingConfiguration: Option[RdsClusterServerlessv2ScalingConfiguration] = None,
+    skipFinalSnapshot: Boolean = false,
     vpcSecurityGroupIds: Option[List[String]] = None,
     databaseName: Option[String] = None,
     backupRetentionPeriod: Int = 5,
     preferredBackupWindow: String = "04:00-06:00",
     tags: Map[String, String] = Map()
-  ) = buildAndAddResource[D, RdsCluster]({ context =>
+  ) = buildAndAddResource[D, RdsCluster]{ context =>
     val b = RdsCluster.Builder
       .create(context.stack, s"$name-rds-cluster")
       .clusterIdentifier(name)
@@ -44,18 +51,40 @@ trait RdsBuilder extends IamBuilder {
       .masterPassword(masterPassword)
       .backupRetentionPeriod(backupRetentionPeriod)
       .preferredBackupWindow(preferredBackupWindow)
+      .skipFinalSnapshot(skipFinalSnapshot)
       .tags(context.mergeTags(name, tags).asJava)
     vpcSecurityGroupIds.map(vpcSecurityGroupIds => b.vpcSecurityGroupIds(vpcSecurityGroupIds.asJava))
     b.dbSubnetGroupName(subnetGroup.getName)
     b.dbClusterParameterGroupName(clusterParameterGroup.getName)
+    engineMode.map(b.engineMode)
+    scalingConfiguration.map(b.scalingConfiguration)
+    serverlessv2ScalingConfiguration.map(b.serverlessv2ScalingConfiguration)
     b
-  }, { (context, resource) =>
-    //    resource.addOverride("babelfish", Map(
-    //      "enabled" -> true,
-    //      "version" -> "0.1.0"
-    //    ).asJava)
-    resource
-  })
+  }
+  
+  def rdsClusterScalingConfiguration(
+    minCapacity: Double,
+    maxCapacity: Double,
+    autoPause: Boolean,
+    secondsUntilAutoPause: Int
+  ) =
+    RdsClusterScalingConfiguration
+      .builder
+      .autoPause(autoPause)
+      .minCapacity(minCapacity)
+      .maxCapacity(maxCapacity)
+      .secondsUntilAutoPause(secondsUntilAutoPause)
+      .build
+
+  def rdsClusterServerlessv2ScalingConfiguration(
+    minCapacity: Double,
+    maxCapacity: Double
+  ) =
+    RdsClusterServerlessv2ScalingConfiguration
+      .builder
+      .minCapacity(minCapacity)
+      .maxCapacity(maxCapacity)
+      .build
 
   def addRdsClusterParameterGroup[D](
     name: String
@@ -83,28 +112,18 @@ trait RdsBuilder extends IamBuilder {
 
   def addRdsClusterInstance[D](
     name: String,
-    cluster: RdsCluster
+    cluster: RdsCluster,
+    instanceClass: String,
+    publiclyAccessible: Boolean = false
   ) =
     buildAndAddResource[D, RdsClusterInstance] { context =>
       RdsClusterInstance.Builder
         .create(context.stack, s"$name-rds-cluster-instance")
+        .identifier(name)
         .clusterIdentifier(cluster.getClusterIdentifier)
-        .engine("aurora-postgresql")
-        .instanceClass("db.r5.large")
-        .publiclyAccessible(true)
-    }
-
-  def addDbInstance[D](
-    name: String,
-    cluster: RdsCluster
-  ) =
-    buildAndAddResource[D, DbInstance] { context =>
-      DbInstance.Builder
-        .create(context.stack, s"$name-rds-cluster-instance")
-        //.(cluster.getClusterIdentifier)
-        //        .engine("aurora-postgresql")
-        //.instanceClass("db.r5.large")
-        .publiclyAccessible(true)
-
+        .engine(cluster.getEngine)
+        .engineVersion(cluster.getEngineVersion)
+        .instanceClass(instanceClass)
+        .publiclyAccessible(publiclyAccessible)
     }
 }
